26d16cdb3a1e6c6d0670c3fd161b1e96b6b5dc71
Johan Runesson <johan.runesson@consid.se>
2020-08-11 16:38:40 +0200

    Adding more macros

--- keyboards/planck/keymaps/overtune/keymap.c
+++ keyboards/planck/keymaps/overtune/keymap.c
@@ -10,27 +10,29 @@ enum custom_keycodes {
 	SE_ARING = SAFE_RANGE,
 	SE_AUML,
 	SE_OUML,
+	MSG_MVH,
+	MSG_EMAIL,
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Layer 0
- * ,---------------------------------------------------------------------------------------------------.
- * | Esc      |  Q   |  W  |  E  |   R   |     T       |     Y      |   U   | I | O |  P |    Bksp     |
- * |----------+------+-----+-----+-------+-------------+------------+-------+---+---+----+-------------|
- * | Ctrl(Tab)|  A   |  S  |  D  |   F   |     G       |     H      |   J   | K | L |  ; |      "      |
- * |----------+------+-----+-----+-------+-------------+------------+-------+---+---+----+-------------|
- * | Shift    |  Z   |  X  |  C  |   V   |     B       |     N      |   M   | , | . |  / |    Enter    |
- * |----------+------+-----+-----+-------+-------------+------------+-------+---+---+----+-------------|
- * | TG(1)    | Ctrl | Alt | CMD | MO(4) | LT_3(Space) | LT_2(Bksp) | MO(4) | * | - | Up | Alt(KC_GRV) |
- * `---------------------------------------------------------------------------------------------------'
+ * ,------------------------------------------------------------------------------------------------------.
+ * | Esc      |  Q   |  W  |  E  |   R   |     T       |     Y      |   U   | I | O |   P   |    Bksp     |
+ * |----------+------+-----+-----+-------+-------------+------------+-------+---+---+-------+-------------|
+ * | Ctrl(Tab)|  A   |  S  |  D  |   F   |     G       |     H      |   J   | K | L |   ;   |      "      |
+ * |----------+------+-----+-----+-------+-------------+------------+-------+---+---+-------+-------------|
+ * | Shift    |  Z   |  X  |  C  |   V   |     B       |     N      |   M   | , | . |   /   |    Enter    |
+ * |----------+------+-----+-----+-------+-------------+------------+-------+---+---+-------+-------------|
+ * | TG(1)    | Ctrl | Alt | CMD | MO(4) | LT_3(Space) | LT_2(Bksp) | MO(4) | * | - | MO(5) | Alt(KC_GRV) |
+ * `------------------------------------------------------------------------------------------------------'
  */
 [0] = LAYOUT_ortho_4x12(
 	KC_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,         KC_Y,          KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, 
 	LCTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,         KC_H,          KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
 	KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,         KC_N,          KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, 
-	TG(1),          KC_LCTL, KC_LALT, KC_LGUI, MO(4), LT(3,KC_SPC), LT(2,KC_BSPC), MO(2), KC_ASTR, KC_MINS, KC_UP,   LALT(KC_GRV)
+	TG(1),          KC_LCTL, KC_LALT, KC_LGUI, MO(4), LT(3,KC_SPC), LT(2,KC_BSPC), MO(2), KC_ASTR, KC_MINS, MO(5),   LALT(KC_GRV)
 ),
 
 /* Layer 1
@@ -53,15 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 /* Layer 2
- * ,-----------------------------------------------.
- * | ▽ | ! | @ | { | } |   |   |   | 7 | 8 | 9 |   |
- * |---+---+---+---+---+---+---+---+---+---+---+---|
- * | ▽ | # | $ | ( | ) |   |   |   | 4 | 5 | 6 |   |
- * |---+---+---+---+---+---+---+---+---+---+---+---|
- * | ▽ | % | ^ | [ | ] |   |   |   | 1 | 2 | 3 |   |
- * |---+---+---+---+---+---+---+---+---+---+---+---|
- * |   |   |   |   | ▽ | ▽ | ▽ | ▽ | - | 0 | = |   |
- * `-----------------------------------------------'
+ * ,------------------------------------------------.
+ * | ▽ | ! | @ | { | } |   |   |   | 7 | 8 | 9  |   |
+ * |---+---+---+---+---+---+---+---+---+---+----+---|
+ * | ▽ | # | $ | ( | ) |   |   |   | 4 | 5 | 6  |   |
+ * |---+---+---+---+---+---+---+---+---+---+----+---|
+ * | ▽ | % | ^ | [ | ] |   |   |   | 1 | 2 | 3  |   |
+ * |---+---+---+---+---+---+---+---+---+---+----+---|
+ * |   |   |   |   | ▽ | ▽ | ▽ | ▽ | - | 0 | =+ |   |
+ * `------------------------------------------------'
  */
 
 [2] = LAYOUT_ortho_4x12(
@@ -71,11 +73,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_P0, KC_EQL, KC_NO
 ),
 
+/* Layer 3
+ * ,----------------------------------------------------------------------------------------------------------------------.
+ * | ▽ |   |  Mouse 1   |  Mouse Up  |   Mouse 2   |   |      |   Mouse Acc 0    |  Mouse Acc 1   | Mouse Acc 2 | =+ |    |
+ * |---+---+------------+------------+-------------+---+------+------------------+----------------+-------------+----+----|
+ * | ▽ |   | Mouse Left | Mouse Down | Mouse Right |   | Left |       Down       |       Up       |    Right    |    | `~ |
+ * |---+---+------------+------------+-------------+---+------+------------------+----------------+-------------+----+----|
+ * | ▽ |   |            |            |             |   |      | Mouse Wheel Down | Mouse Wheel Up |             | -  |    |
+ * |---+---+------------+------------+-------------+---+------+------------------+----------------+-------------+----+----|
+ * |   | ▽ |     ▽      |     ▽      |      ▽      |   |  ▽   |        ▽         |                |             |    |    |
+ * `----------------------------------------------------------------------------------------------------------------------'
+ */
 [3] = LAYOUT_ortho_4x12(
-	KC_ESC,  KC_NO,   KC_BTN1, KC_MS_U, KC_BTN2, KC_NO, KC_NO,   KC_ACL0, KC_ACL1, KC_ACL2, KC_EQL,  KC_NO, 
-	KC_LCTL, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_GRV, 
-	KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_WH_D, KC_WH_U, KC_NO,   KC_MINS, KC_NO, 
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_BSPC, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO
+	KC_TRNS, KC_NO,   KC_BTN1, KC_MS_U, KC_BTN2, KC_NO, KC_NO,   KC_ACL0, KC_ACL1, KC_ACL2, KC_EQL,  KC_NO, 
+	KC_TRNS, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_GRV, 
+	KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_WH_D, KC_WH_U, KC_NO,   KC_MINS, KC_NO, 
+	KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO
 ),
 
 [4] = LAYOUT_ortho_4x12(
@@ -86,10 +99,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [5] = LAYOUT_ortho_4x12(
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	MSG_MVH, MSG_EMAIL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+	KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+	KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+	KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 [6] = LAYOUT_ortho_4x12(
@@ -120,7 +133,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				// when keycode QMKBEST is released
 			}
 			break;
-
+		case MSG_MVH:
+			if (record->event.pressed) {
+				SEND_STRING("Med v"SS_DOWN(X_LALT)"u"SS_UP(X_LALT)"anliga h"SS_DOWN(X_LALT)"u"SS_UP(X_LALT)"alsningar, Johan Runesson");
+				break;
+			}
+		case MSG_EMAIL:
+			if (record->event.pressed) {
+				SEND_STRING("info@johanrunesson.se");
+				break;
+			}
 	}
 	return true;
 };
